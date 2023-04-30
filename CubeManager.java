b8dcc68b15ef8e6bb5c2c02cca0b46599730cb19
FILIPPOS-ATHANASOPOULOS-ANTYRAS <filipposfivos@gmail.com>
2023-04-30 18:07:11 +0300

    initial commit

---
+++ CubeManager.java
@@ -0,0 +1,15 @@
+public class CubeManager {
+    private CubeMatrix cubeMatrix;
+
+    public CubeManager(CubeMatrix cubeMatrix) {
+        this.cubeMatrix = cubeMatrix;
+    }
+    
+    public void createCubeMaxtrix(int numOfCubes) {
+        cubeMatrix = new CubeMatrix();
+        
+
+
+
+    }
+}
---
+++ CubeMatrix.java
@@ -0,0 +1,5 @@
+import java.util.List;
+
+public class CubeMatrix {
+    private List<CubeLine> cubeLines;
+}
