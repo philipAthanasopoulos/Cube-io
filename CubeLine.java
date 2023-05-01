5ee09774fbe51b1bf76d9cad6e7e494ba019bc7f
FILIPPOS-ATHANASOPOULOS-ANTYRAS <filipposfivos@gmail.com>
2023-05-01 18:20:57 +0300

    initialized some methods for the cube manager

--- CubeLine.java
+++ CubeLine.java
@@ -2,5 +2,9 @@ import java.util.List;
 
 public class CubeLine {
     private List<Cube> cubes;
+
+    public boolean isInOrder() {
+        return false;
+    }
     
 }
--- CubeMatrix.java
+++ CubeMatrix.java
@@ -1,5 +1,22 @@
 import java.util.List;
 
 public class CubeMatrix {
+    public CubeMatrix(int numOfCubesPerLine) {
+    }
+
     private List<CubeLine> cubeLines;
+
+    public boolean isMoveable(Cube cube, int xNext, int yNext) {
+        return false;
+    }
+
+    public void moveCube(Cube cube, int xNext, int yNext) {
+    }
+
+    public CubeLine[] getCubeLines() {
+        return null;
+    }
+
+    public void printCubeMatrix() {
+    }
 }
